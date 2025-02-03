529c5e824d5aaf39ab205245a5f814cf119e5d27
Younggwan <kimyogun78@naver.com>
2025-02-03 20:12:33 +0900

    feat: Quiz Entity생성

--- src/main/java/com/LearnDocker/LearnDocker/Quiz.java
+++ src/main/java/com/LearnDocker/LearnDocker/Quiz.java
@@ -1,4 +1,21 @@
 package com.LearnDocker.LearnDocker;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name="Quiz")
+@Getter
+@Setter
 public class Quiz {
+    @Id
+    private int id;
+    @NonNull
+    @Column(name="content")
+    private String content;
 }
