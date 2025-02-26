797697cc0b5ba3e66e161bb92cd86331cb1f8143
Younggwan <kimyogun78@naver.com>
2025-02-27 01:05:26 +0900

    chore: 디렉터리 역할에 맞게 이동

--- src/main/java/com/LearnDocker/LearnDocker/Initializer.java
+++ src/main/java/com/LearnDocker/LearnDocker/Initializer.java
@@ -1,5 +1,6 @@
 package com.LearnDocker.LearnDocker;
 
+import com.LearnDocker.LearnDocker.Configuration.SessionConfig;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 public class Initializer extends AbstractHttpSessionApplicationInitializer {
