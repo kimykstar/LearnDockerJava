314b4d61c13c58857e485c683b0beffeb4c54437
Younggwan <kimyogun78@naver.com>
2025-02-27 01:46:19 +0900

    refactor: 강제 형변환 Objects.toString으로 변경

--- src/main/java/com/LearnDocker/LearnDocker/SandboxController.java
+++ src/main/java/com/LearnDocker/LearnDocker/SandboxController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value="api/sandbox")
@@ -32,7 +33,7 @@ public class SandboxController {
     public void releaseUserSession(final HttpServletRequest httpRequest) {
         final HttpSession session = httpRequest.getSession();
         // 이렇게 Object객체를 String으로 강제 형변환 하는게 좋은 방법인지 알아보기
-        this.sandboxService.releaseUserSession((String)session.getAttribute("containerId"));
+        this.sandboxService.releaseUserSession(Objects.toString(session.getAttribute("containerId"), null));
         session.invalidate();
     }
 
