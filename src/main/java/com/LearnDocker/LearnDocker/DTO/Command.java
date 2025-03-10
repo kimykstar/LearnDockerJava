4c621557ffd6a55d523ffbfc2fad7dcd6b7c9067
Younggwan <kimyogun78@naver.com>
2025-03-11 01:24:18 +0900

    feat: 사용자 입력 명령 전달을 위한 Command Dto객체 생성

---
+++ src/main/java/com/LearnDocker/LearnDocker/DTO/Command.java
@@ -0,0 +1,12 @@
+package com.LearnDocker.LearnDocker.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Command {
+    private String command;
+
+    public Command() {}
+}
