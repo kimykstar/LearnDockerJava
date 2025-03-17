cf1db9e796432b01244665bacc250c7c519c6cc9
Younggwan <kimyogun78@naver.com>
2025-03-17 19:02:04 +0900

    refactor: 사용자 명령 유효성 검증 validator정규 표현식 수정

--- src/main/java/com/LearnDocker/LearnDocker/CommandValidator.java
+++ src/main/java/com/LearnDocker/LearnDocker/CommandValidator.java
@@ -3,9 +3,9 @@ package com.LearnDocker.LearnDocker;
 import java.util.regex.Pattern;
 
 public class CommandValidator {
-    private static final Pattern VALID_REGEX = Pattern.compile("^docker\s(?:(?!.*\b(?:sh|bash|exec|tag|commit)\b|.*[;&|<>$`]))");
-    private static final Pattern VOLUME_MOUNT_CHECK_REGEX =
-                Pattern.compile("^docker\s+(?:(?:container\s+)?(?:create|run))\s+.*(?:-v\b|--volume\b)");
+    private static final Pattern VALID_REGEX = Pattern.compile("^docker\\s(?:(?!.*(?:sh|bash|exec|tag|commit)\\b|.*[;&|<>$`]).)+$");
+    private static final Pattern OPTION_CHECK_REGEX =
+                Pattern.compile("^docker\s+(?:(?:container\s+)?(?:create|run))\s+.*(?:-v\s|--volume\s|-p\s)");
     private static final Pattern PUSH_CHECK_REGEX = Pattern.compile("^docker\s+(?:(?:image\s+)?(?:push))");
     private static final Pattern DOCKER_RUN_CHECK_REGEX = Pattern.compile("^docker\s+(?:(?:container\s+)?(?:run))");
     private static final Pattern DETACH_OPTION_REGEX = Pattern.compile("(?:-d\b|--detach\b)");
@@ -15,7 +15,7 @@ public class CommandValidator {
         command = command.trim();
 
         boolean baseValidation = VALID_REGEX.matcher(command).find();
-        boolean volumeMountValidation = VOLUME_MOUNT_CHECK_REGEX.matcher(command).matches();
+        boolean volumeMountValidation = OPTION_CHECK_REGEX.matcher(command).find();
         boolean pushValidation = PUSH_CHECK_REGEX.matcher(command).find();
         boolean isValid = baseValidation && !volumeMountValidation && !pushValidation;
         if (!isValid) {
