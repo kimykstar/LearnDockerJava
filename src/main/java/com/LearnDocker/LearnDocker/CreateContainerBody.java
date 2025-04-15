a1d413162fb61cbf1b2f6e13ea4336d9d00d503e
Younggwan <kimyogun78@naver.com>
2025-04-15 22:08:13 +0900

    feat: Local Registry접속을 위한 Config변경

--- src/main/java/com/LearnDocker/LearnDocker/CreateContainerBody.java
+++ src/main/java/com/LearnDocker/LearnDocker/CreateContainerBody.java
@@ -3,7 +3,7 @@ package com.LearnDocker.LearnDocker;
 import java.util.*;
 
 public record CreateContainerBody(String Image, HostConfig HostConfig, List<String> Env, List<String> Cmd) {
-    public record HostConfig(boolean Privileged, Map<String, List<PortBinding>> PortBindings) {
+    public record HostConfig(boolean Privileged, Map<String, List<PortBinding>> PortBindings, List<String> ExtraHosts) {
 
     }
 
